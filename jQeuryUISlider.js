764d0284e2b04d87f75c492292a161d58d394ae9
TrywaR <TrywaR@yandex.ru>
2019-06-04 03:04:10 +0300

    add jqeury ui

---
+++ jQeuryUISlider.js
@@ -0,0 +1,31 @@
+// html
+// <div id="price_slider"></div>
+// <input id="filter_price_from" type="text" name="price_from" value="0">
+// <input id="filter_price_to" type="text" name="price_to" value="100000">
+// html x
+
+$(function(){
+  // price_slider
+  var max_value = 100000;
+  $( "#price_slider" ).slider({
+    values: [ 0, max_value ],
+    max: max_value,
+    change: function( event, ui ) {
+      var values = $( "#price_slider" ).slider( "values" )
+      $('#filter_price_from').val(values[0])
+      $('#filter_price_to').val(values[1])
+      $('.ajax-start').click()
+    }
+  });
+
+  const filter_price_to = document.querySelector('#filter_price_to')
+  const filter_price_from = document.querySelector('#filter_price_from')
+
+  filter_price_to.addEventListener('input', updatePriceSliderValue)
+  filter_price_from.addEventListener('input', updatePriceSliderValue)
+
+  function updatePriceSliderValue(e) {
+    $('#price_slider').slider('option', 'values', [ $('#filter_price_from').val(), $('#filter_price_to').val() ]);
+  }
+  // price_slider x
+})
